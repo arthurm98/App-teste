5b15b933c1608141c68169594e7c16a88345bfba
Arthur M <arthurmezzodecarli@gmail.com>
2025-10-04 21:57:41 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,10 @@
 // This makes the app more portable and secure, ready for deployment on platforms like Vercel.
 
 export const firebaseConfig = {
-  projectId: "manga-track-app",
-  appId: "1:1012781212345:web:abcdef1234567890",
-  apiKey: "AIzaSyABCDE1234567890abcdefghijklmno",
-  authDomain: "manga-track-app.firebaseapp.com",
-  storageBucket: "manga-track-app.appspot.com",
-  messagingSenderId: "1012781212345",
+  "projectId": "studio-8995337347-e8395",
+  "appId": "1:646973369859:web:ddaabfe8d0788125ea2836",
+  "apiKey": "AIzaSyDR27-9owIes6DJtZvp-i8AS_KnH9Naixc",
+  "authDomain": "studio-8995337347-e8395.firebaseapp.com",
+  "storageBucket": "studio-8995337347-e8395.appspot.com",
+  "messagingSenderId": "646973369859",
 };
