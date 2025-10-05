c62df77be773837110b117e3109d6799362c507f
Arthur M <arthurmezzodecarli@gmail.com>
2025-10-05 17:16:58 +0000

    Remover apenas na versão Mobile, na versão Desktop manter (_for element

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -80,9 +80,7 @@ function UserNav() {
 export function Header() {
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center justify-between border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6 lg:justify-end">
-        <div className="md:hidden">
-            <SidebarTrigger />
-        </div>
+        
         <div className="flex items-center gap-4">
             <ThemeToggle />
             <UserNav />
